13949588f4d48e0d0a68058b3cf6f2eb7d123afe
Sofiagiraldo01 <Sgiraldo_34@cue.edu.co>
2026-05-22 18:06:02 -0500

    ProductoTest.java

--- src/test/java/com/libreria/ProductoTest.java
+++ src/test/java/com/libreria/ProductoTest.java
@@ -1,4 +1,40 @@
 package com.libreria;
 
+import com.libreria.model.Producto;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ProductoTest {
+
+    @Test
+    void deberiaCrearProductoValido() {
+
+        Producto producto = new Producto("Libro", 100);
+
+        assertEquals(100, producto.getPrecioBase());
+    }
+
+    @Test
+    void deberiaLanzarErrorSiPrecioEsCero() {
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Producto("Libro", 0)
+        );
+
+        assertEquals(
+                "El precio base debe ser mayor que cero",
+                exception.getMessage()
+        );
+    }
+
+    @Test
+    void deberiaLanzarErrorSiPrecioEsNegativo() {
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new Producto("Libro", -10)
+        );
+    }
 }
